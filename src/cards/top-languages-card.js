4e25e446190c7d7efe678730f0adc7fae2baa19f
SasakiSaki <galaster@foxmail.com>
2020-08-12 00:18:54 +0800

    Update top-languages-card.js

--- src/cards/top-languages-card.js
+++ src/cards/top-languages-card.js
@@ -75,7 +75,7 @@ const renderTopLanguages = (topLangs, options = {}) => {
   } = options;
 
   let langs = Object.values(topLangs);
-  let langsToHide = {};
+  let langsToHide = {"html"};
 
   // populate langsToHide map for quick lookup
   // while filtering out
